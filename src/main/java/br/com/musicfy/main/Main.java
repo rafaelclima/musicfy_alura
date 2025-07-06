e5408beaee2a8079359bf3fd7d82ee4cd4f35e14
rafaelclima <rafaelclima.ti@gmail.com>
2025-07-06 13:10:26 -0300

    Feat: adjust UX

--- src/main/java/br/com/musicfy/main/Main.java
+++ src/main/java/br/com/musicfy/main/Main.java
@@ -81,95 +81,118 @@ public class Main {
   }
 
   private void cadastrarArtistas() {
-    System.out.println("\n****************************");
-    System.out.println("*** Cadastro de Artistas ***");
-    System.out.println("****************************");
-    System.out.print("Digite o nome do artista: ");
-    String nome = leitura.nextLine();
+    var sairDoCadastro = false;
+    while (!sairDoCadastro) {
+      System.out.println("\n****************************");
+      System.out.println("*** Cadastro de Artistas ***");
+      System.out.println("****************************");
+      System.out.print("Digite o nome do artista: ");
+      String nome = leitura.nextLine();
 
-    GeneroMusical genero = null;
-    while (genero == null) {
-      System.out.print("Digite o gênero do artista: ");
-      System.out.println("Gêneros disponíveis:");
-      for (GeneroMusical generoMusical : GeneroMusical.values()) {
-        System.out.println("- " + generoMusical);
+      GeneroMusical genero = null;
+      while (genero == null) {
+        System.out.print("Digite o gênero do artista: ");
+        System.out.println("Gêneros disponíveis:");
+        for (GeneroMusical generoMusical : GeneroMusical.values()) {
+          System.out.println("- " + generoMusical);
+        }
+        String generoInput = leitura.nextLine().toUpperCase();
+        try {
+          genero = GeneroMusical.valueOf(generoInput);
+        } catch (IllegalArgumentException e) {
+          System.out.println("Gênero inválido. Por favor, digite um gênero válido.");
+        }
       }
-      String generoInput = leitura.nextLine().toUpperCase();
-      try {
-        genero = GeneroMusical.valueOf(generoInput);
-      } catch (IllegalArgumentException e) {
-        System.out.println("Gênero inválido. Por favor, digite um gênero válido.");
+
+      TipoArtista tipo = null;
+      while (tipo == null) {
+        System.out.print("\nDigite o tipo do artista (SOLO, BANDA, DUPLA): ");
+        String tipoInput = leitura.nextLine().toUpperCase();
+        try {
+          tipo = TipoArtista.valueOf(tipoInput);
+        } catch (IllegalArgumentException e) {
+          System.out.println("Tipo inválido. Por favor, digite um tipo válido.");
+        }
       }
+
+      Artista novoArtista = new Artista(nome, tipo, genero);
+      repositorio.save(novoArtista);
+
+      System.out.println("\n********************************");
+      System.out.println("\nArtista cadastrado com sucesso!");
+      System.out.println("\n********************************\n");
+
+      System.out.println("\nDeseja cadastrar mais artistas? (S/N)");
+      String resposta = leitura.nextLine().toUpperCase();
+      if (!resposta.equals("S")) {
+        sairDoCadastro = true;
+      }
+
     }
 
-    TipoArtista tipo = null;
-    while (tipo == null) {
-      System.out.print("\nDigite o tipo do artista (SOLO, BANDA, DUPLA): ");
-      String tipoInput = leitura.nextLine().toUpperCase();
-      try {
-        tipo = TipoArtista.valueOf(tipoInput);
-      } catch (IllegalArgumentException e) {
-        System.out.println("Tipo inválido. Por favor, digite um tipo válido.");
-      }
-    }
-    System.out.println("\n********************************");
-
-    Artista novoArtista = new Artista(nome, tipo, genero);
-    repositorio.save(novoArtista);
-    System.out.println("\nArtista cadastrado com sucesso!");
-    System.out.println("\n********************************\n");
   }
 
   private void cadastrarMusicas() {
-    System.out.println("\n****************************");
-    System.out.println("*** Cadastro de Músicas ***");
-    System.out.println("****************************");
+    var sairDoCadastro = false;
+    while (!sairDoCadastro) {
+      System.out.println("\n****************************");
+      System.out.println("*** Cadastro de Músicas ***");
+      System.out.println("****************************");
 
-    System.out.print("Digite o nome da música: ");
-    String nome = leitura.nextLine();
-
-    System.out.print("Digite o nome do álbum: ");
-    String album = leitura.nextLine();
-
-    Integer duracaoInt = null;
-    while (duracaoInt == null) {
-      System.out.print("Digite a duração da música (em segundos): ");
-      String input = leitura.nextLine();
-      try {
-        duracaoInt = Integer.parseInt(input);
-      } catch (NumberFormatException e) {
-        System.out.println("Duração inválida! Digite apenas números.");
+      System.out.println("\nEssa música será cadastrada para qual artista?");
+      System.out.println("Artistas cadastrados no banco de dados:");
+      for (Artista artista : repositorio.findAll()) {
+        System.out.println("- [" + artista.getId() + "] " + artista.getNome());
       }
-    }
 
-    System.out.println("Artistas cadastrados:");
-    for (Artista artista : repositorio.findAll()) {
-      System.out.println("- [" + artista.getId() + "] " + artista.getNome());
-    }
-
-    Long idArtista = null;
-    while (idArtista == null) {
-      System.out.print("Digite o ID do artista: ");
-      try {
-        idArtista = Long.parseLong(leitura.nextLine());
-      } catch (NumberFormatException e) {
-        System.out.println("ID inválido! Digite um número.");
+      Long idArtista = null;
+      while (idArtista == null) {
+        System.out.print("\nDigite o ID do artista: ");
+        try {
+          idArtista = Long.parseLong(leitura.nextLine());
+        } catch (NumberFormatException e) {
+          System.out.println("ID inválido! Digite um número.");
+        }
       }
+
+      Optional<Artista> artistaOptional = repositorio.findById(idArtista);
+      if (artistaOptional.isEmpty()) {
+        System.out.println("Artista não encontrado.");
+        return;
+      }
+
+      System.out.print("Digite o nome da música: ");
+      String nome = leitura.nextLine();
+
+      System.out.print("Digite o nome do álbum: ");
+      String album = leitura.nextLine();
+
+      Integer duracaoInt = null;
+      while (duracaoInt == null) {
+        System.out.print("Digite a duração da música (em segundos): ");
+        String input = leitura.nextLine();
+        try {
+          duracaoInt = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+          System.out.println("Duração inválida! Digite apenas números.");
+        }
+      }
+
+      Artista artista = artistaOptional.get();
+      Musica novaMusica = new Musica(nome, album, duracaoInt, artista);
+      musicaRepository.save(novaMusica);
+
+      System.out.println("\n********************************");
+      System.out.println("Música cadastrada com sucesso!");
+      System.out.println("********************************");
+
+      System.out.println("Deseja cadastrar mais músicas? (S/N)");
+      String resposta = leitura.nextLine();
+      if (!resposta.equalsIgnoreCase("S")) {
+        sairDoCadastro = true;
+      }
+
     }
-
-    Optional<Artista> artistaOptional = repositorio.findById(idArtista);
-    if (artistaOptional.isEmpty()) {
-      System.out.println("Artista não encontrado.");
-      return;
-    }
-
-    Artista artista = artistaOptional.get();
-    Musica novaMusica = new Musica(nome, album, duracaoInt, artista);
-    musicaRepository.save(novaMusica);
-
-    System.out.println("\n********************************");
-    System.out.println("Música cadastrada com sucesso!");
-    System.out.println("********************************");
   }
 
   private void listarMusicas() {
@@ -181,12 +204,13 @@ public class Main {
       System.out.println("Nenhuma música encontrada.");
       return;
     }
-    System.out.println("Lista de músicas:");
     for (Musica musica : musicas) {
+      System.out.println("\n**********************************************");
       System.out.println("Título: " + musica.getTitulo());
       System.out.println("Álbum: " + musica.getAlbum());
       System.out.println("Duração: " + musica.getDuracao() + " segundos");
       System.out.println("Artista: " + musica.getArtista().getNome());
+      System.out.println("**********************************************");
     }
   }
 
